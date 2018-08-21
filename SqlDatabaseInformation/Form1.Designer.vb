45eff90e4e3bdf3fecda63e84e4c8873b02ecbfb
Manvinder Singh <manvindersingh@outlook.com>
2018-08-21 15:27:27 +0530

    Initial commit

---
+++ SqlDatabaseInformation/Form1.Designer.vb
@@ -0,0 +1,85 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtConnectionString = New System.Windows.Forms.TextBox()
+        Me.btnLoad = New System.Windows.Forms.Button()
+        Me.lstTables = New System.Windows.Forms.ListBox()
+        Me.lstColumns = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'txtConnectionString
+        '
+        Me.txtConnectionString.Location = New System.Drawing.Point(32, 13)
+        Me.txtConnectionString.Name = "txtConnectionString"
+        Me.txtConnectionString.Size = New System.Drawing.Size(676, 20)
+        Me.txtConnectionString.TabIndex = 0
+        '
+        'btnLoad
+        '
+        Me.btnLoad.Location = New System.Drawing.Point(715, 13)
+        Me.btnLoad.Name = "btnLoad"
+        Me.btnLoad.Size = New System.Drawing.Size(75, 30)
+        Me.btnLoad.TabIndex = 1
+        Me.btnLoad.Text = "&Load"
+        Me.btnLoad.UseVisualStyleBackColor = True
+        '
+        'lstTables
+        '
+        Me.lstTables.FormattingEnabled = True
+        Me.lstTables.Location = New System.Drawing.Point(32, 59)
+        Me.lstTables.Name = "lstTables"
+        Me.lstTables.Size = New System.Drawing.Size(376, 355)
+        Me.lstTables.TabIndex = 2
+        '
+        'lstColumns
+        '
+        Me.lstColumns.FormattingEnabled = True
+        Me.lstColumns.Location = New System.Drawing.Point(414, 59)
+        Me.lstColumns.Name = "lstColumns"
+        Me.lstColumns.Size = New System.Drawing.Size(374, 355)
+        Me.lstColumns.TabIndex = 3
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.lstColumns)
+        Me.Controls.Add(Me.lstTables)
+        Me.Controls.Add(Me.btnLoad)
+        Me.Controls.Add(Me.txtConnectionString)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Database Information"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtConnectionString As TextBox
+    Friend WithEvents btnLoad As Button
+    Friend WithEvents lstTables As ListBox
+    Friend WithEvents lstColumns As ListBox
+End Class
---
+++ SqlDatabaseInformation/Form1.vb
@@ -0,0 +1,51 @@
+﻿Imports System.Data.SqlClient
+
+Public Class Form1
+
+    Dim _connection As SqlConnection
+
+
+    Private Sub btnLoad_Click(sender As Object, e As EventArgs) Handles btnLoad.Click
+        If (String.IsNullOrWhiteSpace(txtConnectionString.Text)) Then
+            MessageBox.Show("Please paste valid connection string")
+            Exit Sub
+        End If
+
+        _connection = New SqlConnection(Me.txtConnectionString.Text)
+        Try
+            Dim adapter As New SqlDataAdapter("select name,id from sysobjects where type='u' and name!='sysdiagrams'", _connection)
+            Dim dt As New DataTable
+            adapter.Fill(dt)
+
+            Me.lstTables.DisplayMember = "name"
+            Me.lstTables.ValueMember = "name"
+            Me.lstTables.DataSource = dt
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+
+        End Try
+
+
+    End Sub
+
+    Private Sub lstTables_SelectedValueChanged(sender As Object, e As EventArgs) Handles lstTables.SelectedValueChanged
+        If lstTables.SelectedValue Is Nothing Then
+            Exit Sub
+        End If
+
+        Try
+            Dim adapter As New SqlDataAdapter("select * from INFORMATION_SCHEMA.COLUMNS where table_name='" + lstTables.SelectedValue + "'", _connection)
+            Dim dt As New DataTable
+            adapter.Fill(dt)
+
+            Me.lstColumns.DisplayMember = "column_name"
+            Me.lstColumns.DataSource = dt
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+
+        End Try
+
+    End Sub
+End Class
